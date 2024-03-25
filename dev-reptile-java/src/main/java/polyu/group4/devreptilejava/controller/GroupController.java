c84250cb3ecf689e51e3a09e86b17942d73dea30
rockyTTTT <107424045+rockyTTTT@users.noreply.github.com>
2024-03-25 20:23:50 +0800

    规范项目结构，并且完成提交次数的方法编写。

---
+++ dev-reptile-java/src/main/java/polyu/group4/devreptilejava/controller/GroupController.java
@@ -0,0 +1,20 @@
+package polyu.group4.devreptilejava.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import polyu.group4.devreptilejava.service.impl.GroupService;
+
+@RestController
+@RequestMapping("/api/groups")
+public class GroupController {
+    @Autowired
+    private GroupService groupService;
+
+    @GetMapping("/commits")
+    public String getGroupCommits(){
+        return groupService.getGroupCommits();
+    }
+}
